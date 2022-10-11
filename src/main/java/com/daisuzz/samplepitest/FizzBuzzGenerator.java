04570ddd87a325c141376a7297fa0f7a1011f93b
motakashi <takashi_home_sky@yahoo.co.jp>
2022-10-12 00:46:15 +0900

    firstCommit

---
+++ src/main/java/com/daisuzz/samplepitest/FizzBuzzGenerator.java
@@ -0,0 +1,21 @@
+package com.daisuzz.samplepitest;
+
+public class FizzBuzzGenerator {
+
+    public String generate(int number) {
+
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        }
+
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+
+        return String.valueOf(number);
+    }
+}
